5fb556fd098f4c345e8605e3eececf6fe05395e2
Gjenis Alkovaç <76169691+Gjenis87@users.noreply.github.com>
2022-04-28 18:47:22 +0200

    Experimenting with javascript

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 let uc = require("upper-case");
 console.log(uc.upperCase("test"));
+
+let a=3;
+let b="3";
+console.log(a==b);
+console.log(a===b);
+
+let myWord="Gjenis";
+if(myWord=="Gjenis"&&myWord==="Gjeni"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+let myVar=3;
+if(myVar==3){
+    console.log("sukses");
+}else{
+    console.log("gabim");
+}
+
+if(true){
+    let myNum=1;
+    console.log(myNum);
+    if(myNum==1){
+        console.log("Urime keni fiTUAR NI KG Miell");
+    }
+}
+console.log(a);
+
+
